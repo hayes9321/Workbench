f071c0f6a5c873486fa5c8e71c575ac8a656c017
hayes9321 <t.hayes8321@gmail.com>
2017-01-03 12:25:00 -0800

    updated singlylinkedlist

--- linkedList/DoublyLinkedList.js
+++ linkedList/DoublyLinkedList.js
@@ -1,9 +1,9 @@
 //Building A Doubly Linked List.
 
 /*
- A doubly linked list takes all the funcitonality of a singly-linked list and extends it for bi-directional movement 
- in a list(it a head, and a tail pointer). We can traverse, in other words, a linked list from the first node in the list
- to the last node in the list; and we can traverse from the last node in the list to the first node in the list. 
+	A doubly linked list takes all the funcitonality of a singly-linked list and extends it for bi-directional movement 
+	in a list(it a head, and a tail pointer). We can traverse, in other words, a linked list from the first node in the list
+	to the last node in the list; and we can traverse from the last node in the list to the first node in the list. 
 
 
 Operations of a Doubly-linked list
@@ -63,55 +63,55 @@ DoublyList.prototype.add = function(value){
 
 //remove from an instance of a DoublylinkedList
 DoublyList.prototype.remove = function(position) {
- var currentNode = this.head,
-  length = this._length,
-  count = 1,
-  message = {failure: 'Failure: non-existent node in this list.'},
-  beforeNodeToDelete = null,
-  nodeToDelete = null,
-  deletedNode = null;
+	var currentNode = this.head;
+	var length = this.length;
+	var count = 1;
+	var message = {failure: 'Failure: non-existent node in this list.'};
+	var beforeNodeToDelete = null;
+	var nodeToDelete = null;
+	var deletedNode = null;
 
- // 1st use-case: an invalid position
- if (length === 0 || position < 1 || position > length) {
-  throw new Error(message.failure);
- }
+	// 1st use-case: an invalid position
+	if (length === 0 || position < 1 || position > length) {
+		throw new Error(message.failure);
+	}
 
-	 // 2nd use-case: the first node is removed
-	 if (position === 1) {
-   this.head = currentNode.next;
+		// 2nd use-case: the first node is removed
+		if (position === 1) {
+			this.head = currentNode.next;
 
-   // 2nd use-case: there is a second node
-   if (!this.head) {
-    this.head.previous = null;
-   // 2nd use-case: there is no second node
-   } else {
-   	this.tail = null;
-   }
+			// 2nd use-case: there is a second node
+			if (!this.head) {
+				this.head.previous = null;
+			// 2nd use-case: there is no second node
+			} else {
+				this.tail = null;
+			}
 
- // 3rd use-case: the last node is removed
- } else if (position === this._length) {
-  this.tail = this.tail.previous;
-  this.tail.next = null;
- // 4th use-case: a middle node is removed
- } else {
-  while (count < position) {
-      currentNode = currentNode.next;
-      count++;
-  }
+	// 3rd use-case: the last node is removed
+	} else if (position === this.length) {
+		this.tail = this.tail.previous;
+		this.tail.next = null;
+	// 4th use-case: a middle node is removed
+	} else {
+		while (count < position) {
+						currentNode = currentNode.next;
+						count++;
+		}
 
-  beforeNodeToDelete = currentNode.previous;
-  nodeToDelete = currentNode;
-  afterNodeToDelete = currentNode.next;
+		beforeNodeToDelete = currentNode.previous;
+		nodeToDelete = currentNode;
+		afterNodeToDelete = currentNode.next;
 
-  beforeNodeToDelete.next = afterNodeToDelete;
-  afterNodeToDelete.previous = beforeNodeToDelete;
-  deletedNode = nodeToDelete;
-  nodeToDelete = null;
- }
+		beforeNodeToDelete.next = afterNodeToDelete;
+		afterNodeToDelete.previous = beforeNodeToDelete;
+		deletedNode = nodeToDelete;
+		nodeToDelete = null;
+	}
 
- this._length--;
+	this.length--;
 
- return message.success;
+	return message.success;
 };
 
 //remove from an instance of a DoublyLinkedList
