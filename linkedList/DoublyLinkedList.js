4319c389e9609596f4e607dc834707655c81e9eb
hayes9321 <t.hayes8321@gmail.com>
2017-01-03 12:37:00 -0800

    updated singlylinkedlist

--- linkedList/DoublyLinkedList.js
+++ linkedList/DoublyLinkedList.js
@@ -1,9 +1,9 @@
 //Building A Doubly Linked List.
 
 /*
-	A doubly linked list takes all the funcitonality of a singly-linked list and extends it for bi-directional movement 
-	in a list(it a head, and a tail pointer). We can traverse, in other words, a linked list from the first node in the list
-	to the last node in the list; and we can traverse from the last node in the list to the first node in the list. 
+  A doubly linked list takes all the funcitonality of a singly-linked list and extends it for bi-directional movement 
+  in a list(it a head, and a tail pointer). We can traverse, in other words, a linked list from the first node in the list
+  to the last node in the list; and we can traverse from the last node in the list to the first node in the list. 
 
 
 Operations of a Doubly-linked list
@@ -25,111 +25,111 @@ DoublyList
 
 //Node class
 function Node(value){
-	this.val = value;
-	this.previous = null;
-	this.next = null;
+  this.val = value;
+  this.previous = null;
+  this.next = null;
 }
 
 //Doubly linked list class
 function DoublyList(){
-	this.length = 0;
-	this.head = null;
-	this.tail = null;
+  this.length = 0;
+  this.head = null;
+  this.tail = null;
 }
 
 //Add to an instance of a DoublylinkedList
 DoublyList.prototype.add = function(value){
-	//add a new instance of a node class
-	var node = new Node(value)
-	//if there are nodes in the list then the value of head is null
-	var currentNode  = this.head; 
+  //add a new instance of a node class
+  var node = new Node(value)
+  //if there are nodes in the list then the value of head is null
+  var currentNode  = this.head; 
 
-	//1st Case: Empty List, no current node 
-	if(!currentNode) {
-		//assign node as the head of the list
-		this.head = node;
-		//increment its length 
-		this.length++;
-	} 
-	//2nd Case: Non empty list //There is a current node
-	while(currentNode.next){
-		currentNode = currentNode.next;
-	}
+  //1st Case: Empty List, no current node 
+  if(!currentNode) {
+    //assign node as the head of the list
+    this.head = node;
+    //increment its length 
+    this.length++;
+  } 
+  //2nd Case: Non empty list //There is a current node
+  while(currentNode.next){
+    currentNode = currentNode.next;
+  }
 
-	currentNode.next = node;
-	this.length++;
-	return node;
+  currentNode.next = node;
+  this.length++;
+  return node;
 }
 
 //remove from an instance of a DoublylinkedList
 DoublyList.prototype.remove = function(position) {
-	var currentNode = this.head;
-	var length = this.length;
-	var count = 1;
-	var message = {failure: 'Failure: non-existent node in this list.'};
-	var beforeNodeToDelete = null;
-	var nodeToDelete = null;
-	var deletedNode = null;
+  var currentNode = this.head;
+  var length = this.length;
+  var count = 1;
+  var message = {failure: 'Failure: non-existent node in this list.'};
+  var beforeNodeToDelete = null;
+  var nodeToDelete = null;
+  var deletedNode = null;
 
-	// 1st use-case: an invalid position
-	if (length === 0 || position < 1 || position > length) {
-		throw new Error(message.failure);
-	}
+  // 1st use-case: an invalid position
+  if (length === 0 || position < 1 || position > length) {
+    throw new Error(message.failure);
+  }
 
-		// 2nd use-case: the first node is removed
-		if (position === 1) {
-			this.head = currentNode.next;
+    // 2nd use-case: the first node is removed
+    if (position === 1) {
+      this.head = currentNode.next;
 
-			// 2nd use-case: there is a second node
-			if (!this.head) {
-				this.head.previous = null;
-			// 2nd use-case: there is no second node
-			} else {
-				this.tail = null;
-			}
+      // 2nd use-case: there is a second node
+      if (!this.head) {
+        this.head.previous = null;
+      // 2nd use-case: there is no second node
+      } else {
+        this.tail = null;
+      }
 
-	// 3rd use-case: the last node is removed
-	} else if (position === this.length) {
-		this.tail = this.tail.previous;
-		this.tail.next = null;
-	// 4th use-case: a middle node is removed
-	} else {
-		while (count < position) {
-						currentNode = currentNode.next;
-						count++;
-		}
+  // 3rd use-case: the last node is removed
+  } else if (position === this.length) {
+    this.tail = this.tail.previous;
+    this.tail.next = null;
+  // 4th use-case: a middle node is removed
+  } else {
+    while (count < position) {
+            currentNode = currentNode.next;
+            count++;
+    }
 
-		beforeNodeToDelete = currentNode.previous;
-		nodeToDelete = currentNode;
-		afterNodeToDelete = currentNode.next;
+    beforeNodeToDelete = currentNode.previous;
+    nodeToDelete = currentNode;
+    afterNodeToDelete = currentNode.next;
 
-		beforeNodeToDelete.next = afterNodeToDelete;
-		afterNodeToDelete.previous = beforeNodeToDelete;
-		deletedNode = nodeToDelete;
-		nodeToDelete = null;
-	}
+    beforeNodeToDelete.next = afterNodeToDelete;
+    afterNodeToDelete.previous = beforeNodeToDelete;
+    deletedNode = nodeToDelete;
+    nodeToDelete = null;
+  }
 
-	this.length--;
+  this.length--;
 
-	return message.success;
+  return message.success;
 };
 
 //remove from an instance of a DoublyLinkedList
 DoublyList.prototype.searchNodeAt = function(value){
-	var currentNode = this.head;
-	var length = this.length;
-	var count = 1;
-	var message = {failure: 'Failure: non-existent node in this list.'};
-	//1st Case: invalid position
-	if(length === 0 || position < 1 || postion > length){
-		throw new Error(message.failure);
-	}
-	//2nd Case: a valid posiiton
-	while(count < postion){
-		currentNode = currentNode.next;
-		count++;
-	}
-	return currentNode;
+  var currentNode = this.head;
+  var length = this.length;
+  var count = 1;
+  var message = {failure: 'Failure: non-existent node in this list.'};
+  //1st Case: invalid position
+  if(length === 0 || position < 1 || postion > length){
+    throw new Error(message.failure);
+  }
+  //2nd Case: a valid posiiton
+  while(count < postion){
+    currentNode = currentNode.next;
+    count++;
+  }
+  return currentNode;
 };
 
 
--- linkedList/singlyLinkedList.js
+++ linkedList/singlyLinkedList.js
@@ -1,12 +1,12 @@
 //Creating a singly linked list
 
 /*
-	To create a linked list the first step is to define
-	a node object that will point to the next node. 
+  To create a linked list the first step is to define
+  a node object that will point to the next node. 
 
-	For the linked list it should have a ponter to point to the node 
-	of the linked list, It should have methods for
-	adding and removing nodes from the list. 
+  For the linked list it should have a ponter to point to the node 
+  of the linked list, It should have methods for
+  adding and removing nodes from the list. 
 */
 
 /*First, create object that has one property head(a pointer)
@@ -15,70 +15,70 @@ of the object
 */
 //Create Node class
 function Node(value){
-	this.value = val;
-	this.next = null;
+  this.value = val;
+  this.next = null;
 }
 
 function SinglyList(){
-	this.head = null;
-	this.length = 0;
+  this.head = null;
+  this.length = 0;
 }
 
 /*
-	Add method will take value and will create a node object. If there
-	is no head present then the value will be assigned as the head.	
+  Add method will take value and will create a node object. If there
+  is no head present then the value will be assigned as the head. 
 */
 
 LinkedList.prototype.add = function(val){
-	var node = new Node();
-	var currentNode = this.head;
+  var node = new Node();
+  var currentNode = this.head;
 
-	//1st case: an empty list
-	if(!currentNode){
-		this.head = node;
-		this.length++;
+  //1st case: an empty list
+  if(!currentNode){
+    this.head = node;
+    this.length++;
 
-		return node;
-	}
-	//2nd case a non-empty list
-	while(currentNode.next){
-		currentNode = currentNode.next;
-	}
-	currentNode.next = node;
-	this.length++;
+    return node;
+  }
+  //2nd case a non-empty list
+  while(currentNode.next){
+    currentNode = currentNode.next;
+  }
+  currentNode.next = node;
+  this.length++;
 
-	return node;
+  return node;
 
 //Next add remove class, includes 3 remove cases.
 LinkedList.prototype.remove = function(val){
-	var current = this.head;
+  var current = this.head;
 
-	//The targeted node is in the head. Replace head with the next node.
-	if(current.value == val){
-		this.head = current.next;
-	}
+  //The targeted node is in the head. Replace head with the next node.
+  if(current.value == val){
+    this.head = current.next;
+  }
 
-	//The targeted node is in the middle of a LinkedList, you have to make the node 
-	// after your taregted node before the tail will be null.
-	else{
-		var previous = current;
+  //The targeted node is in the middle of a LinkedList, you have to make the node 
+  // after your taregted node before the tail will be null.
+  else{
+    var previous = current;
 
-		//loop through linked list heads
-		while(current.next){
-			if(current.value == val){
-				previous.next = current.next;
-				break;
-			}
-			previous = current;
-			current = current.next;
-		}
+    //loop through linked list heads
+    while(current.next){
+      if(current.value == val){
+        previous.next = current.next;
+        break;
+      }
+      previous = current;
+      current = current.next;
+    }
 
-		//The targeted node is in the tail. You just have to remove it from 
-		//the tail. Hence next of the node before hte tail will be null
-		if(current.value == val){
-			previous.next == null;
-		}
-	}
+    //The targeted node is in the tail. You just have to remove it from 
+    //the tail. Hence next of the node before hte tail will be null
+    if(current.value == val){
+      previous.next == null;
+    }
+  }
 }
 
 //Create an instance of a linked list and add values.
