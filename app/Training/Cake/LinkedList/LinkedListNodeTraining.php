4bc992462501df6884f2faa175ed8a677055b646
Travis Hayes <you@example.com>
2020-11-13 20:02:49 -0800

    wip

--- app/Training/Cake/LinkedList/LinkedListNodeTraining.php
+++ app/Training/Cake/LinkedList/LinkedListNodeTraining.php
@@ -14,7 +14,7 @@ class LinkedListNodeTraining
     | Use the class below
     |
     */
-    public static function handle()
+    public function handle()
     {
         // SETUP
         $a = new LinkedListNode('A');
@@ -24,31 +24,28 @@ class LinkedListNodeTraining
         $a->setNext($b);
         $b->setNext($c);
 
-        // Solution: space and time complexity o(1)
-        function deleteNode($node)
-        {
-            // get the input node's next node, the one we want to skip to
-            $nextNode = $node->getNext();
-
-            if ($nextNode) {
-
-                // replace the input node's value and pointer with the next
-                // node's value and pointer. the previous node now effectively
-                // skips over the input node
-                $node->setValue($nextNode->getValue());
-                $node->setNext($nextNode->getNext());
-
-            } else {
-
-                // eep, we're trying to delete the last node!
-                throw new Exception("Can't delete the last node with this technique!");
-            }
-        }
-
         // RUN
-        deleteNode($b);
+        $this->deleteNode($b);
 
         dd($a);
+    }
+
+    // Solution: space and time complexity o(1)
+    public function deleteNode($node)
+    {
+        // get the input node's next node, the one we want to skip to
+        $nextNode = $node->getNext();
+
+        if ($nextNode) {
+            // replace the input node's value and pointer with the next
+            // node's value and pointer. the previous node now effectively
+            // skips over the input node
+            $node->setValue($nextNode->getValue());
+            $node->setNext($nextNode->getNext());
+        } else {
+            // eep, we're trying to delete the last node!
+            throw new Exception("Can't delete the last node with this technique!");
+        }
     }
 }
 
