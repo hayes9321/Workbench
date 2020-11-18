e5bdb4f56519a00e7228b1ff60c9975a818f23ab
Travis Hayes <you@example.com>
2020-11-18 09:44:00 -0800

    wip

---
+++ app/Training/Cake/Arr/ProductOfOtherNumbersTraining.php
@@ -0,0 +1,58 @@
+<?php
+
+namespace App\Training\Cake\Arr;
+
+class ProductOfOtherNumbersTraining
+{
+    /*
+    |--------------------------------------------------------------------------
+    | Training Description
+    |--------------------------------------------------------------------------
+    |
+    | You have an array of integers, and for each index you want to find the product of every integer except the integer at that index.
+    |
+    | Write a function getProductsOfAllIntsExceptAtIndex() that takes an array of integers and returns an array of the products.
+    |
+    | For example, given:
+    |
+    |    [1, 7, 3, 4]
+    |
+    | your function would return:
+    |
+    |    [84, 12, 28, 21]
+    |
+    | by calculating:
+    |
+    |    [7 * 3 * 4,  1 * 3 * 4,  1 * 7 * 4,  1 * 7 * 3]
+    |
+    | Here's the catch: You can't use division in your solution!
+    |
+    */
+
+
+    public function handle()
+    {
+        $ints = [1, 7, 3, 4];
+
+        $result = $this->getProductsOfAllIntsExceptAtIndex($ints);
+
+        dd($result);
+    }
+
+    public function getProductsOfAllIntsExceptAtIndex($numbers)
+    {
+        $productsArray = [];
+
+        for ($i = 0; $i < count($numbers); $i++) {
+            $filterArray = $numbers; // new array, do not want to modify existing array.
+
+            unset($filterArray[$i]); // remove the value from the newly created array
+
+            $calculatedProduct = array_product($filterArray); // calculate the product of the new array
+
+            array_push($productsArray, $calculatedProduct); // push to the array that will be return.
+        }
+
+        return $productsArray;
+    }
+}
--- class-map.php
+++ class-map.php
@@ -11,11 +11,12 @@
 */
 
 return [
-    'LinkedListNodeTraining'    => 'App\Training\Cake\LinkedList\LinkedListNodeTraining',
-    'LinkedList'                => 'App\Toolbelt\DataStructure\LinkedList',
-    'DoublyLinkedList'          => 'App\Toolbelt\DataStructure\DoublyLinkedList',
-    'LinkedListCyclesTraining'  => 'App\Training\Cake\LinkedList\LinkedListCyclesTraining',
-    'ReverseLinkedListTraining' => 'App\Training\Cake\LinkedList\ReverseLinkedListTraining',
-    'KthToLastNodeTraining'     => 'App\Training\Cake\LinkedList\KthToLastNodeTraining',
-    'AppleStockTraining'        => 'App\Training\Cake\Arr\AppleStockTraining'
+    'LinkedListNodeTraining'        => 'App\Training\Cake\LinkedList\LinkedListNodeTraining',
+    'LinkedList'                    => 'App\Toolbelt\DataStructure\LinkedList',
+    'DoublyLinkedList'              => 'App\Toolbelt\DataStructure\DoublyLinkedList',
+    'LinkedListCyclesTraining'      => 'App\Training\Cake\LinkedList\LinkedListCyclesTraining',
+    'ReverseLinkedListTraining'     => 'App\Training\Cake\LinkedList\ReverseLinkedListTraining',
+    'KthToLastNodeTraining'         => 'App\Training\Cake\LinkedList\KthToLastNodeTraining',
+    'AppleStockTraining'            => 'App\Training\Cake\Arr\AppleStockTraining',
+    'ProductOfOtherNumbersTraining' => 'App\Training\Cake\Arr\ProductOfOtherNumbersTraining'
 ];
